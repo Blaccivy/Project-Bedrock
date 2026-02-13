060f7e472a25817d8b5a2fa18c0580548753ad77
Shoyoye Dotun <shoyoyedotun032@gmail.com>
2026-02-13 11:02:06 +0100

    feature updated

--- aws-auth.tf
+++ aws-auth.tf
@@ -1,11 +1,3 @@
-# # provider "kubernetes" {
-# #   host                   = data.aws_eks_cluster.this.endpoint
-# #   cluster_ca_certificate = base64decode(
-# #     data.aws_eks_cluster.this.certificate_authority[0].data
-# #   )
-# #   token = data.aws_eks_cluster_auth.this.token
-# # }
-
 resource "kubernetes_config_map_v1" "aws_auth" {
   metadata {
     name      = "aws-auth"
